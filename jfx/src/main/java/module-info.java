f422fbdb28f8eb0ab4f0042fa073e069f4e9e833
metoa25 <teomateo25@gmail.com>
2024-06-16 13:48:32 +0200

    Version 0.1
    Added Everything

---
+++ jfx/src/main/java/module-info.java
@@ -0,0 +1,17 @@
+module org.mateo.jfx {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+    requires java.sql;
+
+    opens org.mateo.jfx to javafx.fxml;
+    exports org.mateo.jfx;
+}
